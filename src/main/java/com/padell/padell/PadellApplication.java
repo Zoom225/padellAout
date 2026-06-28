2e1096babeda474b2ee47e640762de3e33a955f2
mise-a-jour-backend <bobypro225@gmail.com>
2026-06-28 07:53:48 +0200

    mise en place projet

---
+++ src/main/java/com/padell/padell/PadellApplication.java
@@ -0,0 +1,13 @@
+package com.padell.padell;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PadellApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PadellApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/padell/padell/PadellApplicationTests.java
@@ -0,0 +1,13 @@
+package com.padell.padell;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PadellApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
